12a75f1eeffdbbb43da8f6610b3f8af8bc3d48fd
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-29 19:53:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon = {}
+  data.each do |characteristics , value|
+    value.each do | key, results|
+      results.each do |name|
+        if pigeon.has_key?(name)
+          if pigeon[name].has_key?(characteristics)
+            pigeon[name][characteristics] << key.to_s
+          else
+            pigeon[name][characteristics] = [key.to_s]
+          end
+        else
+          pigeon[name] = {characteristics => [key.to_s]}
+        end
+      end 
+    end
+  end
+  pigeon
 end
