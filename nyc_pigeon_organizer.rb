905257d583d58017eede8185ab106329e2825b80
Linh Huynh <35814458+linh4@users.noreply.github.com>
2018-07-31 01:00:35 -0400

    Update nyc_pigeon_organizer.rb

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  data.each { |key, value|
-    value.each { |describe, array|
-      array.each { |name|
+  data.each do |key, value|
+    value.each do |describe, array|
+      array.each do |name|
         if pigeon_list.has_key?(name)
           if pigeon_list[name].has_key?(key)
             pigeon_list[name][key] << describe.to_s
@@ -13,8 +13,8 @@ def nyc_pigeon_organizer(data)
         else
           pigeon_list[name] = {key => [describe.to_s]}
         end
-      }
-    }
-  }
+      end
+    end
+  end
   pigeon_list
 end
